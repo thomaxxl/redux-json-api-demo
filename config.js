f54d6f9c0440418dbe39e5a5a70d77259d84ddf1
Thomas POLLET <thomas.pollet@gmail.com>
2018-03-21 16:19:53 +0100

    update

---
+++ config.js
@@ -0,0 +1,5 @@
+var baseUrl = 'http://thomaxxl.pythonanywhere.com'
+var collection_name = 'Users'
+var collection_endpoint = collection_name + '/?page[limit]=100'
+
+column_names = ['','name','email']
--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware, combineReducers } from 'redux'
-import { connect } from 'react-redux';
-import thunk from 'redux-thunk';
-import { reducer as api, readEndpoint } from 'redux-json-api';
-import { setAxiosConfig } from 'redux-json-api';
-import { Button, ButtonToolbar } from 'react-bootstrap/lib/Button';
-import Alert from 'react-bootstrap/lib/Alert';
-import Badge from 'react-bootstrap/lib/Badge';
-import Table from 'react-bootstrap/lib/Table';
+import { connect } from 'react-redux'
+import thunk from 'redux-thunk'
+import { reducer as api, readEndpoint } from 'redux-json-api'
+import { setAxiosConfig } from 'redux-json-api'
+import { Button, ButtonToolbar } from 'react-bootstrap/lib/Button'
+import Alert from 'react-bootstrap/lib/Alert'
+import Badge from 'react-bootstrap/lib/Badge'
+import Glyphicon from 'react-bootstrap/lib/Glyphicon'
+import Table from 'react-bootstrap/lib/Table'
+
+const queryString = require('query-string')
 
 /*
     Configuration parameters
 */
 
-var baseUrl = 'http://thomaxxl.pythonanywhere.com';
-var collection_name = 'Users';
-var collection_endpoint = collection_name + '/?page[limit]=100'
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const reducer = combineReducers({
     api
-});
+})
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
@@ -31,60 +31,144 @@ store.dispatch(setAxiosConfig({
     headers: {
         'Authorization': 'bearer' + Math.random(),
     }
-}));
+}))
+
+
+class ObjectAction extends React.Component {
+
+    render_action(object_id){
+        return null
+    }
+
+    render(){
+        return <span className="object-action"> {this.render_action(this.props.object_id)} </span>
+    }
+}
+
+
+class DeleteAction extends ObjectAction {
+
+    render_action(object_id){
+        return <Glyphicon glyph="trash" />
+    }   
+}
+
+
+class CreateAction extends ObjectAction {
+
+    render_action(object_id){
+        return <Glyphicon glyph="pencil" />
+    }   
+}
 
 
 class ObjectTable extends React.Component {
 
-    componentWillMount() {
-        store.dispatch(readEndpoint(collection_endpoint));
+    constructor(props) {
+        super(props)
+        this.state = {
+            column_names : column_names,
+            actions : [CreateAction, DeleteAction]
+        }
     }
 
-    render() {
-        let column_names = [];
-        let objects = this.props.api_objects.data;
+    componentWillMount() {
+        store.dispatch(readEndpoint(collection_endpoint))   
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        /*
+            If the column_names have been set that means we don't need to set them again
+            (otherwise we may run into recursion issues through setState<>componentDidUpdate)
+        */
+        if(!this.state.column_names.length){
+            this.getColumnNames()
+        }
+    }
+
+    getColumnNames(){
+        let column_names = []
+        let objects = this.props.api_objects.data
         if(objects){
             /*
                 We use the first object from the collection as a sample object so we can render 
                 the attribute keys as column names
             */
-            let sample_object = objects[0];
+            let sample_object = objects[0]
             if(sample_object && sample_object.attributes){
-               column_names = Object.keys(sample_object.attributes) ;
+               column_names = Object.keys(sample_object.attributes)
             }
         }
+        if(column_names.length){
+            this.setState({'column_names' : column_names})
+        }
+    }
+
+    render_header(){
+        return this.state.column_names.map(col_name => 
+                                <th key={col_name}>{ col_name }</th>
+               )
+    }
+
+    render_rows(){
+        return this.props.api_objects.data.map(api_object =>
+                            <ObjectRow api_object={api_object} 
+                                       key={api_object.id} 
+                                       parent={this} />
+               )
+    }
+
+    render_action(action, object_id){
+        const Action = action
+        return <Action object_id={object_id} key={action}/>
+    }
+
+    render_actions(object_id){
+
+        if(!this.state.actions){
+            return null
+        }
+
+        let actions_col = <td>
+                            {this.state.actions.map(action => 
+                                this.render_action(action, object_id)
+                            )}
+                        </td>
+
+        return actions_col
+    }
+
+    render() {
 
         return (
             <div>
                 <Table striped bordered condensed hover>
                     <thead>
                         <tr>
-                            {column_names.map(col_name => 
-                                <th key={col_name}>{ col_name }</th>
-                            )}
+                            {this.render_header()}
                         </tr>
                     </thead>
 
                     <tbody>
-                        {this.props.api_objects.data.map(api_object => (
-                            <ObjectRow api_object={api_object} key={api_object.id} />
-                        ))}
+                        {this.render_rows()}
                     </tbody>
                 </Table>
             </div>
-        );
+        )
     }
-};
+}
 
 
 class ObjectRow extends React.Component {
 
     render() {
+        let column_names = this.props.parent.state.column_names
         return(
             <tr>
-                {Object.keys(this.props.api_object.attributes).map(attr =>
-                    <ObjectCol attribute={this.props.api_object.attributes[attr]} key={attr}/>
-                    )}
+                {this.props.parent.render_actions(this.props.api_object.id)}
+                {column_names.map(col_name => 
+                    <ObjectCol attribute={this.props.api_object.attributes[col_name]} key={col_name}/>
+                )}
             </tr>
         )
     }
@@ -94,20 +178,21 @@ class ObjectRow extends React.Component {
 class ObjectCol extends React.Component {
 
     render() {
-        return(
-            <td>{this.props.attribute}</td>
-        )
+        if (typeof this.props.attribute !== 'undefined' ) {
+            return <td>{this.props.attribute}</td>
+        }
+        return null
     }
 }
 
 
 const mapStateToProps = (state) => {
     console.log(state) // Check the console to see how the state object changes as we read the API
-    const api_objects = state.api[collection_name] || { data: [] };
+    const api_objects = state.api[collection_name] || { data: [] }
     return {
         api_objects
     }
-};
+}
 
 const ApiResults = connect(
     mapStateToProps
@@ -116,4 +201,4 @@ const ApiResults = connect(
 ReactDOM.render(
     <ApiResults store={store} />,
     document.getElementById('api_objects')
-);
+)
